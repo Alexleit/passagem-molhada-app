1a51fb33c22b372a3132133b649344181f17972e
Alexandre Leite <36412188+Alexleit@users.noreply.github.com>
2026-07-30 22:15:51 -0300

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'br.geophocus.passagemmolhada',
-  appName: 'Vistoria Passagem Molhada',
-  webDir: 'www'
-};
-
-export default config;
